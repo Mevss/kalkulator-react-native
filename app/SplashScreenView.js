039c990194aceee44fd61eaf5a1ab8b1d76f905e
Mevss <147794606+Mevss@users.noreply.github.com>
2024-11-22 01:35:57 +0100

    Update SplashScreenView.js

--- app/SplashScreenView.js
+++ app/SplashScreenView.js
@@ -24,29 +24,3 @@ const styles = StyleSheet.create({
         resizeMode: "cover",
     }
 });
-import { StyleSheet, View, Image } from "react-native"
-import Icon from "../assets/images/calculator.png"
-
-export default function SplashScreen(){
-    return(
-        <View style={styles.container}>
-            <View>
-                <Image source={Icon} style={styles.image}/>
-            </View>
-        </View>
-    )
-}
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#1e1e1e",
-    },
-    image: {
-        width: 100, 
-        height: 100, 
-        resizeMode: "cover",
-    }
-});
